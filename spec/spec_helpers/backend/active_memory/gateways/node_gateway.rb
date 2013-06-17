406af3ba21b32f828123c3f2e8e938c088face9f
Jacob Ashdown <jcbashdown@googlemail.com>
2013-06-18 00:33:06 +0100

    active memory persist for integration tests, implement for neo later

---
+++ spec/spec_helpers/backend/active_memory/gateways/node_gateway.rb
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+
+module Backends
+
+  module ActiveMemory
+
+    class NodeGateway < RubyPersistenceAPI::ActiveMemory::Gateway
+
+      entity_class Entities::Node
+
+    end
+
+  end
+
+end
